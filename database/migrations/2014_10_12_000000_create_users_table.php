7fe641dda47869749bad066ecd721aff09883a51
unknown <abdallakaramdev@gmail.com>
2020-07-21 15:12:34 +0200

    Make Route DashBoard And Design Admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('type',array('admin','user'))->default('admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//['register'=>false]
 
+
+Auth::routes();
 
 Route::group(['namespace'=>'FrontEnd'],function (){
 
     Route::get('/','HomePageController');
 });
-
